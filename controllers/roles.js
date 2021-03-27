fc2ddc778dec5d1eefa118cb252a575fd71e6de7
Linar Valdianov <v.linar0711@mail.ru>
2021-03-27 14:30:35 +0500

    CRUD for roles

--- controllers/roles.js
+++ controllers/roles.js
@@ -4,3 +4,19 @@ const Roles = new Services();
 exports.role = async function (request, response) {
   response.send(await Roles.getallrole());
 };
+
+exports.oneroles = async function (request, response) {
+  response.send(await Roles.getonerole(request.params.id));
+};
+
+exports.updateroles = async (request, response) => {
+  response.send(await Roles.updaterole(request.params.id, request.body));
+};
+
+exports.createroles = async (request, response) => {
+  response.send(await Roles.createrole(request.body));
+};
+
+exports.deleteroles = async (request, response) => {
+  response.send(await Roles.delstatus(request.params.id));
+};
