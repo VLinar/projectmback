3f4e22e4a70796eb5a90267200823614d66e520a
Linar Valdianov <v.linar0711@mail.ru>
2021-03-27 15:23:24 +0500

    CRUD for payment. Fix orders and delivery model

--- services/payment.js
+++ services/payment.js
@@ -16,4 +16,70 @@ module.exports = class Paymentservices {
         };
       });
   };
+  getonepayment = (payid) => {
+    return Payment.findByPk(payid)
+      .then((res) => {
+        return {
+          status: "success",
+          result: res,
+        };
+      })
+      .catch((err) => err);
+  };
+  updatepayment = (payid, data) => {
+    return Payment.update(data, {
+      where: {
+        id: payid,
+      },
+    })
+      .then(async (res) => {
+        const result = await this.getonepayment(payid);
+        return {
+          status: "updated",
+          result: result.result,
+        };
+      })
+      .catch((err) => {
+        err.errors = err.errors.map((error) => {
+          return {
+            type: error.type,
+            message: error.message,
+          };
+        });
+        return {
+          status: "error",
+          errors_msg: err.errors,
+        };
+      });
+  };
+
+  createpayment = (data) => {
+    return Payment.create(data)
+      .then((res) => res)
+      .catch((err) => {
+        err.errors = err.errors.map((error) => {
+          return {
+            type: error.type,
+            message: error.message,
+          };
+        });
+        return err.errors;
+      });
+  };
+
+  delpayment = (deleteid) => {
+    return Payment.destroy({
+      where: {
+        id: deleteid,
+      },
+    })
+      .then((res) => {
+        return res != 0
+          ? {
+              status: "deleted",
+            }
+          : { status: "error", message: "элемент не найден" };
+      })
+      .catch((err) => err);
+  };
 };
